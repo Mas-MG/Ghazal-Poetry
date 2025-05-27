5bdce0fd5dbd8db42aaf0dd5e74566c9b453546b
masoomeh ghorbani <masoomeh77ghorbani98@gmail.com>
2025-05-27 13:39:51 +0330

    Add: Send poem btn

--- src/bot/bot.update.ts
+++ src/bot/bot.update.ts
@@ -1,10 +1,12 @@
-import { Update, On, Ctx } from 'nestjs-telegraf';
+import { Update, On, Ctx, Start, Action } from 'nestjs-telegraf';
 import { Injectable } from '@nestjs/common';
-import { Context } from 'telegraf';
+import { Context, Markup } from 'telegraf';
 import { ConfigService } from '@nestjs/config';
 import { InjectModel } from '@nestjs/mongoose';
 import { Poem } from './schema/bot.schema';
 import { Model } from 'mongoose';
+
+const waitingForPoem = new Map<number, boolean>();
 
 @Update()
 @Injectable()
@@ -13,6 +15,25 @@ export class BotUpdate {
     private readonly config: ConfigService,
     @InjectModel(Poem.name) private readonly poemModel: Model<Poem>,
   ) {}
+
+  @Start()
+  async startCommand(@Ctx() ctx: Context) {
+    await ctx.reply(
+      'خوش اومدی، برای ارسال شعر روی دکمه زیر کلیک کن!',
+      Markup.inlineKeyboard([
+        Markup.button.callback('📝ارسال شعر', 'SEND_POEM'),
+      ]),
+    );
+  }
+
+  @Action('SEND_POEM')
+  async sendPoem(@Ctx() ctx: Context) {
+    if (!ctx.from) return;
+    const userId = ctx.from.id;
+    waitingForPoem.set(userId, true);
+    await ctx.answerCbQuery();
+    await ctx.reply('هرچه دل تنگت میخواهد بگو...');
+  }
 
   @On('text')
   async onText(@Ctx() ctx: Context) {
@@ -27,8 +48,8 @@ export class BotUpdate {
       throw new Error('TELEGRAM_GROUP_ID is not set in environment variables');
     }
 
-    const chatId = message.chat.id;
-    const messageId = message.message_id;
+    // const chatId = message.chat.id;
+    // const messageId = message.message_id;
 
     const { id: userId, username, first_name, last_name } = message.from;
     const { text } = message;
@@ -39,9 +60,8 @@ export class BotUpdate {
       firstName: first_name,
       lastName: last_name,
       text,
-      sent:false
+      sent: false,
     });
-    
 
     // await ctx.telegram.copyMessage(groupId, chatId, messageId);
     await ctx.reply('شعر زیبای شما ارسال شد قشنگم ^^');
